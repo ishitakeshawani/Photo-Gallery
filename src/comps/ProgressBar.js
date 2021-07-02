c9539fb710df2e9ae3f35629baf1ff132444145a
ishitakeshawani <keshwaniishita054@gmail.com>
2021-07-02 22:41:22 +0530

    add animations using motion

--- src/comps/ProgressBar.js
+++ src/comps/ProgressBar.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import useStorage from "../hooks/useStorage";
+import { motion } from 'framer-motion';
 
 
 const ProgressBar = ({file, setFile}) => {
@@ -17,9 +18,12 @@ const ProgressBar = ({file, setFile}) => {
 
 
     return (
-        <div className="progress-bar" style={{ width: progress + '%'}}>
-
-        </div>
+        <motion.div className="progress-bar" 
+        initial={{width:0 }}
+        animate={{ width: progress + '%'}}
+        >
+            
+        </motion.div>
     )
 }
 
