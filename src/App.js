4b90ef4da25d2b295789c0a9a7855d5654a4cc2c
ishitakeshawani <keshwaniishita054@gmail.com>
2021-07-13 20:24:40 +0530

    added route for login

--- src/App.js
+++ src/App.js
@@ -2,17 +2,22 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import SignUp from './comps/signup/SignUp';
 import Home from './comps/Home'; 
+import login from './comps/login/login';
+import { AuthProvider } from './contexts/AuthContexts';
 
 
 function App() {
   return (
     <div className="App">
+      <AuthProvider>
       <Router>
       <Switch>
         <Route exact path="/" component={SignUp} ></Route>
+        <Route path="/login" component={login}></Route>
         <Route path="/home" component={Home}></Route>
       </Switch>
       </Router>
+      </AuthProvider>
     </div>
   );
 }
