58ae5ced6bab06d465b910848decf42e7cac0f34
ishitakeshawani <keshwaniishita054@gmail.com>
2021-07-13 20:26:49 +0530

    small change in UI

--- src/comps/signup/SignUp.js
+++ src/comps/signup/SignUp.js
@@ -2,7 +2,6 @@ import React from "react";
 import Title from "../Title";
 import LottieAnimation from "../Lottie";
 import signup from "../../animation/signup.json";
-import { Row, Container,Col } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import SignUpLogic from "./SignUpLogic";
 
@@ -12,13 +11,11 @@ const SignUp = () => {
     return (
         <div className="div">
             <Title />
-            
-                <Row>
-                    <Col sm={6}><LottieAnimation lotti={signup} height={400} width={500} /></Col>
-                    <Col><SignUpLogic /></Col>
-                </Row>
-          
-            
+            <div className="row">
+                <div class="col d-none d-lg-block"><LottieAnimation lotti={signup} height={400} width={500} /></div>
+                <div class="col"><SignUpLogic /></div>
+            </div>
+               
         </div>
     )
 }
