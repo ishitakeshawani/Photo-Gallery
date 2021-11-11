68f2b0fe8ebcebc325749de0aa6415faa1ab5d69
ishitakeshawani <keshwaniishita054@gmail.com>
2021-11-11 20:57:48 +0530

    improve UI of signup and login page

--- src/comps/navbar/NavBari.js
+++ src/comps/navbar/NavBari.js
@@ -1,27 +1,31 @@
-import  React from "react";
-import { Navbar,Nav, Container,Button } from "react-bootstrap";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import { Navbar, Nav, Container, Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./navbar.css";
-import {Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 const NavBari = () => {
-    return (
-        <Navbar collapseOnselect fixed="Top" expand='xl' bg="#fff">
-            <Container>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="mr-auto">
-                        <Nav.Link href="/">Home</Nav.Link>
-                         
-                    </Nav>
-                    <Nav className="justify-content-end">
-                        <Button variant="outline-info"><Link to="/login">Sign In</Link></Button>&nbsp;
-                        <Button variant="info"><Link to="/">Sign Up</Link></Button>
-                    </Nav> 
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    );
-}
+  return (
+    <Navbar collapseOnselect fixed="Top" expand="xl" bg="#fff">
+      <Container>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+          </Nav>
+          <Nav className="justify-content-end">
+            <Button variant="outline-info">
+              <Link to="/login">Sign In</Link>
+            </Button>
+            &nbsp;
+            <Button variant="info">
+              <Link to="/">Sign Up</Link>
+            </Button>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
 
 export default NavBari;
--- src/comps/signup/SignUpLogic.js
+++ src/comps/signup/SignUpLogic.js
@@ -1,44 +1,38 @@
-import React, { useState} from 'react';
-import { Card, Container } from 'react-bootstrap';
+import React, { useState } from "react";
+import { Card, Container } from "react-bootstrap";
 import { Link, useHistory } from "react-router-dom";
-import { useAuth, } from '../../contexts/AuthContexts';
+import { useAuth } from "../../contexts/AuthContexts";
 import "./signup.css";
-import {  toast } from 'react-toastify';
+import { toast } from "react-toastify";
 toast.configure();
 
 const SignUpLogic = () => {
-    const [username, setUserName] = useState("");
-    const [password, setPassword] = useState("");
-    const [email, setEmail] = useState("");
-    const { signup } = useAuth();
-    let [errors, setErrors] = useState('');
-    const [Loading, setLoading] = useState(false);
-    const history = useHistory();
-    
-    const finalValidate = () => {
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const { signup } = useAuth();
+  let [errors, setErrors] = useState("");
+  const [Loading, setLoading] = useState(false);
+  const history = useHistory();
+
+  const finalValidate = () => {
     errors = {};
 
     if (username === undefined || username === "")
       errors.username = "Please enter your name";
-    else if (
-      username.length < 4
-    )
+    else if (username.length < 4)
       errors.username = "Please enter at least 4 characters";
 
-    if ((email === undefined || email === ""))
+    if (email === undefined || email === "")
       errors.email = "Please enter your email";
     else if (
-      !/^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/.test(
-       email
-      )
+      !/^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/.test(email)
     )
       errors.email = "Please enter a valid username address";
 
-    if (
-        password.length < 6
-      )
-        errors.password = "Please enter at least 6 characters";
-  
+    if (password.length < 6)
+      errors.password = "Please enter at least 6 characters";
+
     setErrors(errors);
 
     if (Object.entries(errors).length === 0) {
@@ -48,16 +42,14 @@ const SignUpLogic = () => {
     }
   };
 
-   
-    async function handleSubmit(e) {
-        e.preventDefault();
-        if (!finalValidate()) return;
-        setLoading(true);
-        
-        try {
-            await signup(email, password);
-            let message =
-        "Sign Up successfully";
+  async function handleSubmit(e) {
+    e.preventDefault();
+    if (!finalValidate()) return;
+    setLoading(true);
+
+    try {
+      await signup(email, password);
+      let message = "Sign Up successfully";
       toast.success(message, {
         position: "top-right",
         autoClose: 0,
@@ -66,94 +58,104 @@ const SignUpLogic = () => {
         pauseOnHover: true,
         draggable: true,
       });
-              
-            setLoading(false);
-            
-            history.push("/home");
 
-        } catch(error) {
-            errors = {};
-            errors.other = error.message;
-            setErrors(errors);
-            setLoading(false);
-            
-        }    
+      setLoading(false);
+
+      history.push("/home");
+    } catch (error) {
+      errors = {};
+      errors.other = error.message;
+      setErrors(errors);
+      setLoading(false);
     }
+  }
 
-    return (
-        <>
-            <Container className="d-flex align-items-center "
-                style={{ minHeight: "80vh" }}
-            >
-                <div className="w-100" style={{ maxWidth: "500px" }}>
-                    <Card>
-                        <Card.Body>
-                            <h2 className="text-center mt-4">Sign Up</h2>
-                            
-                            <form>
-                        <div className="form-group">
-                        <label htmlFor="username">Username</label>
-                        <input
-                            required
-                            type="username"
-                            className="form-control"
-                            name="username"
-                            id="username"
-                            value={username}
-                            
-                            onChange={(e) => {
-                            setUserName(e.target.value);
-                            }}
-            
-                                    />
-                    <span className="form-text text-danger">{errors.username || ""}</span>
-                    </div>
+  return (
+    <>
+      <Container
+        className="d-flex align-items-center "
+        style={{ minHeight: "80vh" }}
+      >
+        <div className="w-100" style={{ maxWidth: "500px" }}>
+          <Card>
+            <Card.Body>
+              <h2 className="text-center mt-4">Sign Up</h2>
+
+              <form>
+                <div className="form-group">
+                  <label htmlFor="username">Username</label>
+                  <input
+                    required
+                    type="username"
+                    className="form-control"
+                    name="username"
+                    id="username"
+                    value={username}
+                    onChange={(e) => {
+                      setUserName(e.target.value);
+                    }}
+                  />
+                  <span className="form-text text-danger">
+                    {errors.username || ""}
+                  </span>
+                </div>
                 <div class="form-group">
-                    <label for="exampleInputEmail1">Email</label>
-                    <input type="email" class="form-control"
-                        name="email"
-                        id="email"
-                        value={email}
-                        onChange={(e) => {
-                        setEmail(e.target.value);
-                        }}
-                    />
-                    <span className="form-text text-danger">{errors.email || ""}</span>
+                  <label for="exampleInputEmail1">Email</label>
+                  <input
+                    type="email"
+                    class="form-control"
+                    name="email"
+                    id="email"
+                    value={email}
+                    onChange={(e) => {
+                      setEmail(e.target.value);
+                    }}
+                  />
+                  <span className="form-text text-danger">
+                    {errors.email || ""}
+                  </span>
                 </div>
                 <div className="form-group">
-                <label htmlFor="password">Password</label>
-                <input type="password" 
+                  <label htmlFor="password">Password</label>
+                  <input
+                    type="password"
                     name="password"
                     value={password}
                     class="form-control"
                     onChange={(e) => {
-                        setPassword(e.target.value);
+                      setPassword(e.target.value);
                     }}
-                    />
-                    <span className="form-text text-danger">{errors.password || ""}</span>
+                  />
+                  <span className="form-text text-danger">
+                    {errors.password || ""}
+                  </span>
                 </div>
-                                <div className="text-center">
-                                <button 
-                            type="button"
-                            onClick={(e) => handleSubmit(e)}
-                            className="btn btn-primary"
-                            disabled={Loading}
-                            >Sign UP
-                            </button>
-                            </div>
-            </form>
-            <span className="form-text text-danger">{errors.other || ""}</span>                       
-                            
-                        </Card.Body>
-                    </Card>
-                    <div className="w-100 text-center mt-2">
-                        Already have an account? <Link to="/login" className="bottomlink">Log In</Link>
-                    </div>
+                <div className="text-center">
+                  <button
+                    type="button"
+                    onClick={(e) => handleSubmit(e)}
+                    className="btn btn-primary"
+                    disabled={Loading}
+                  >
+                    Sign UP
+                  </button>
                 </div>
-            </Container>
-
-        </>
-    );
-}
+              </form>
+              <span className="form-text text-danger">
+                {errors.other || ""}
+              </span>
+            </Card.Body>
+          </Card>
+          <div className="w-100 text-center mt-2">
+            Already have an account?{" "}
+            <Link to="/login" className="bottomlink">
+              Log In
+            </Link>
+          </div>
+        </div>
+      </Container>
+    </>
+  );
+};
 
 export default SignUpLogic;
